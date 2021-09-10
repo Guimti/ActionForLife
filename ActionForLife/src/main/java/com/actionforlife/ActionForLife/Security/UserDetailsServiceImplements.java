709486eeb2c6aab03f2272b2c2035e97e91b5615
evelyncarolina <evelyncarolina.contato@gmail.com>
2021-09-10 11:49:29 -0300

    [Feature Evelyn - Task: Created UserDetailsServiceImplements]

--- ActionForLife/src/main/java/com/actionforlife/ActionForLife/Security/UserDetailsServiceImplements.java
+++ ActionForLife/src/main/java/com/actionforlife/ActionForLife/Security/UserDetailsServiceImplements.java
@@ -1,5 +1,33 @@
 package com.actionforlife.ActionForLife.Security;
 
-public class UserDetailsServiceImplements {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.actionforlife.ActionForLife.Model.UsuarioModel;
+import com.actionforlife.ActionForLife.Repository.UsuarioRepository;
+
+public class UserDetailsServiceImplements implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository repositorio;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<UsuarioModel> objetoOptional = repositorio.findByemail(username);
+		
+		if (objetoOptional.isPresent()) {
+			return new UserDetailsImplements(objetoOptional.get());
+		}
+		else {
+			throw new UsernameNotFoundException(username + "Não Existe!");
+		}
+		
+	} 
+
+	
 }
