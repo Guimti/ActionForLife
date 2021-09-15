50b3324f1e9c0c2f10de698ca2caabf766856712
AlysonRodrigo <alysonrodrigo14@hotmail.com>
2021-09-15 14:50:01 -0300

    [Feaature Alyson - redirect update]

--- ActionForLife/src/main/java/com/actionforlife/ActionForLife/ActionForLifeApplication.java
+++ ActionForLife/src/main/java/com/actionforlife/ActionForLife/ActionForLifeApplication.java
@@ -2,12 +2,20 @@ package com.actionforlife.ActionForLife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/")
 @SpringBootApplication
 public class ActionForLifeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActionForLifeApplication.class, args);
 	}
-
+	
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 }
