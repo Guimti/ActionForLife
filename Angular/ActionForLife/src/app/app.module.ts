9f3ae97c578493977ed70290f18aaa6c339ec474
charlô <charlesoliveira0293@hotmail.com>
2021-10-10 22:57:33 -0300

    [Feature Charles - Task: Creating the route for the About component]

--- Angular/ActionForLife/src/app/app.module.ts
+++ Angular/ActionForLife/src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
-import { CategoryEditComponent } from './edit/category-edit/category-edit.component';
-import { CategoryDeleteComponent } from './delete/category-delete/category-delete.component';
-
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AboutComponent } from './about/about.component';
 
+import { CategoryEditComponent } from './edit/category-edit/category-edit.component';
+import { CategoryDeleteComponent } from './delete/category-delete/category-delete.component';
 import { ProductEditComponent } from './edit/product-edit/product-edit.component';
 import { ProductDeleteComponent } from './delete/product-delete/product-delete.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-
 
 @NgModule({
   declarations: [
@@ -33,9 +32,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
     ProductsComponent,
     CategoryEditComponent,
     CategoryDeleteComponent,
-
-    AboutComponent
-
+    AboutComponent,
     ProductEditComponent,
     ProductDeleteComponent,
     AboutUsComponent
